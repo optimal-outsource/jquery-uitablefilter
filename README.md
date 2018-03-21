a96ebae38d44bf066d42ac8c2c2649b90f34cd93
Adam Stein <adam@csh.rit.edu>
2018-03-21 08:30:46 -0400

    * add ability to specify a callback for getting text from an element (default is to use element.text()) * updated function to be able to pass in an "opts" dictionary instead of each option individually * fixed bugs (determining if column option was given, using size attribute which doesn't exist,   getting last element of array) * fixed syntax issues (using == instead of ===, missing semicolons, that sort of thing)

--- README.md
+++ README.md
@@ -1,19 +1,49 @@
 # Explanation
-This is forked from https://github.com/natinusala/jquery-uitablefilter in order to add a package.json file so it can be loaded via npm with a git url
+This is forked from https://github.com/RobCannon/jquery-uitablefilter in order to:
+
+* add ability to specify a callback for getting text from an element (default is to use element.text())
+* updated function to be able to pass in an "opts" dictionary instead of each option individually
+* fixed bugs (determining if column option was given, using size attribute which doesn't exist, getting last element of array)
+* fixed syntax issues (using == instead of ===, missing semicolons, that sort of thing)
 
 # Original script
 https://github.com/gregwebs/jquery-uitablefilter
 
 ## What's new ?
-With this version the 'column' argument has been modified to be an array of columns instead of being just one column.
-Consequently, the script is now able to search in several columns. If you want the original behaviour back, you can put a simple String.
-If you don't want to create any filter, just put an empty array as argument.
+With this version comes the ability to pass in a function to call to retrieve text from HTML objects in the table. By default, 
+the jQuery text() function is used. This callback function can be used to get text from objects that don't support text(). For example,
+getting the text of the current selection from a menu (\<select\>) doesn't work with text() (you need to get the text from the selected menu option).
+
+In addition, instead of having to specify each option in the uiTableFilter() function call, an "opts" dictionary is passed in containing
+only those options that are set.
 
 ## Download
-Download the latest version here : https://raw.github.com/natinusala/jquery-uitablefilter/master/jquery.uitablefilter.js
+Download the latest version here : https://raw.github.com/optimal-outsource/jquery-uitablefilter/master/jquery.uitablefilter.js
 
 ## Example
-Here is an example (see the original project README for more informations). It will search for the word 'Pepper' in #table in columns 'Price', 'Item' and 'ID'.
+Here is an example (see the original project README for more information). It will search for the word 'Pepper' in #table in columns 'Price', 'Item' and 'ID'.
 ```
-$.uiTableFilter( $("#table"), "Pepper",  ["Price", "Item", "D"]);
+$.uiTableFilter(
+    $("#table"), 
+    "Pepper",
+    {
+        "column": ["Price", "Item", "ID"]
+    }
+);
+```
+
+Here is another example showing how to retrieve text using your own function.
+```
+function myGetText(elem) {
+    // Return text from a <select> drop down menu
+    return elem.find("option:selected").text().trim();
+}
+
+$.uiTableFilter(
+    $("#table"),
+    "Pepper",
+    {
+        "getTextCallback": myGetText
+    }
+)
 ```
--- jquery.uitablefilter.js
+++ jquery.uitablefilter.js
@@ -5,8 +5,14 @@
  *
  * Multi-columns fork by natinusala 
  *
+ * NPM support fork by RobCannon
+ *
+ * getText callback fork by optimal-outsource
+ *
  * documentation at http://gregweber.info/projects/uitablefilter
  *                  https://github.com/natinusala/jquery-uitablefilter
+ *                  https://github.com/RobCannon/jquery-uitablefilter
+ *                  https://github.com/optimal-outsource/jquery-uitablefilter
  *
  * allows table rows to be filtered (made invisible)
  * <code>
@@ -17,97 +23,114 @@
  *   jQuery object containing table rows
  *   phrase to search for
  *   optional arguments:
- *     array of columns to limit search too (the column title in the table header)
- *     ifHidden - callback to execute if one or more elements was hidden
+ *      opts - dictionary of optional arguments:
+ *          column          - array of columns to limit search too (the column title in the table header)
+ *          getTextCallback - function to retrieve text from element
+ *          ifHidden        - callback to execute if one or more elements was hidden
  */
 (function($) {
-  $.uiTableFilter = function(jq, phrase, column, ifHidden){
-    var new_hidden = false;
-    if( this.last_phrase === phrase ) return false;
+    var options = {
+        column: [],
+        getTextCallback: null,
+        ifHidden: null
+    };
 
-    var phrase_length = phrase.length;
-    var words = phrase.toLowerCase().split(" ");
+    $.uiTableFilter = function(jq, phrase, opts) {
+        options = $.extend(options, opts || {});
 
-    // these function pointers may change
-    var matches = function(elem) { elem.show() }
-    var noMatch = function(elem) { elem.hide(); new_hidden = true }
-    var getText = function(elem) { return elem.text() }
+        var new_hidden = false;
+        if( this.last_phrase === phrase ) return false;
 
-    if( column ) 
+        var phrase_length = phrase.length;
+        var words = phrase.toLowerCase().split(" ");
+
+        // these function pointers may change
+        var matches = function(elem) { elem.show() };
+        var noMatch = function(elem) { elem.hide(); new_hidden = true };
+        var getText = (options["getTextCallback"]) ? function(elem) { return options["getTextCallback"](elem) }
+                                                   : function(elem) { return elem.text() };
+
+        if( options["column"].length )
+        {
+            if (!$.isArray(options["column"]))
+            {
+                options["column"] = new Array(options["column"]);
+            }
+
+            var index = [];
+
+            jq.find("thead > tr:last > th").each(function(i)
+            {
+                for (var j = 0; j < options["column"].length; j++)
+                {
+                    if ($.trim($(this).text()) === options["column"][j])
+                    {
+                        index[j] = i;
+                        break;
+                    }
+                }
+
+            });
+
+            getText = function(elem) {
+                var selector = "";
+                for (var i = 0; i < index.length; i++)
+                {
+                    if (i !== 0) {selector += ",";}
+                    selector += "td:eq(" + index[i] + ")";
+                }
+
+                if (options["getTextCallback"] === null)
+                {
+                    return $(elem.find((selector))).text();
+                }
+                else
+                {
+                    return options["getTextCallback"]($(elem.find((selector))));
+                }
+            }
+        }
+
+        // if added one letter to last time,
+        // just check newest word and only need to hide
+        if( (words.length > 1) && (phrase.substr(0, phrase_length - 1) === this.last_phrase) ) {
+            if( phrase[phrase_length - 1] === " " )
+            { this.last_phrase = phrase; return false; }
+
+            words = words[words.length - 1]; // just search for the newest word
+
+            // only hide visible rows
+            matches = function(elem) {};
+            var elems = jq.find("tbody:first > tr:visible");
+        }
+        else {
+            new_hidden = true;
+            elems = jq.find("tbody:first > tr");
+        }
+
+        elems.each(function(){
+            var elem = $(this);
+            $.uiTableFilter.has_words( getText(elem), words, false ) ?
+                matches(elem) : noMatch(elem);
+        });
+
+        this.last_phrase = phrase;
+        if( options["ifHidden"] && new_hidden ) options["ifHidden"]();
+        return jq;
+    };
+
+    // caching for speedup
+    $.uiTableFilter.last_phrase = "";
+
+    // not jQuery dependent
+    // "" [""] -> Boolean
+    // "" [""] Boolean -> Boolean
+    $.uiTableFilter.has_words = function( str, words, caseSensitive )
     {
-      if (!$.isArray(column))
-      {
-        column = new Array(column);
-      }
-	  
-      var index = new Array(); 
-      
-      jq.find("thead > tr:last > th").each(function(i) 
-      {
-          for (var j = 0; j < column.length; j++)
-          {     
-              if ($.trim($(this).text()) == column[j])
-              {
-                  index[j] = i;
-                  break;
-              }           
-          }
-
-      });
-         
-      getText = function(elem) {
-          var selector = "";
-          for (var i = 0; i < index.length; i++)
-          {
-              if (i != 0) {selector += ",";}
-              selector += "td:eq(" + index[i] + ")";
-          }
-          return $(elem.find((selector))).text();
-      }
+        var text = caseSensitive ? str : str.toLowerCase();
+        for (var i=0; i < words.length; i++) {
+            if (text.indexOf(words[i]) === -1) return false;
+        }
+        return true;
     }
-
-    // if added one letter to last time,
-    // just check newest word and only need to hide
-    if( (words.size > 1) && (phrase.substr(0, phrase_length - 1) ===
-          this.last_phrase) ) {
-
-      if( phrase[-1] === " " )
-      { this.last_phrase = phrase; return false; }
-
-      var words = words[-1]; // just search for the newest word
-
-      // only hide visible rows
-      matches = function(elem) {;}
-      var elems = jq.find("tbody:first > tr:visible")
-    }
-    else {
-      new_hidden = true;
-      var elems = jq.find("tbody:first > tr")
-    }
-
-    elems.each(function(){
-      var elem = $(this);
-      $.uiTableFilter.has_words( getText(elem), words, false ) ?
-        matches(elem) : noMatch(elem);
-    });
-
-    last_phrase = phrase;
-    if( ifHidden && new_hidden ) ifHidden();
-    return jq;
-  };
-
-  // caching for speedup
-  $.uiTableFilter.last_phrase = ""
-
-  // not jQuery dependent
-  // "" [""] -> Boolean
-  // "" [""] Boolean -> Boolean
-  $.uiTableFilter.has_words = function( str, words, caseSensitive )
-  {
-    var text = caseSensitive ? str : str.toLowerCase();
-    for (var i=0; i < words.length; i++) {
-      if (text.indexOf(words[i]) === -1) return false;
-    }
-    return true;
-  }
 }) (jQuery);
